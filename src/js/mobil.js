e5301b19aebe2f6e6800e0f191f8e081bdd0ede3
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-05-25 13:36:48 +0200

    якорные ссылки и скрипт закрывающий модалку при нажатии на ссылку

--- src/js/mobil.js
+++ src/js/mobil.js
@@ -1,13 +1,18 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-    const mobileBtnClose = document.querySelector("[data-menu-close]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  
-    mobileBtnClose.addEventListener("click", () => {
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })();
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
+  const mobileList = document.querySelector(".mobil-list");
+
+  menuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  mobileBtnClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+
+  mobileList.addEventListener("click", () => {
+    mobileMenuRef.classList.remove("is-open");
+  });
+})();
